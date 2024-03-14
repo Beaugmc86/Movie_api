c0452f497668fca6ddd7d97cfb8b7ebfb98adb12
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-03-14 13:56:34 -0700

    update mongoose.connect

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { 
-mongoose.connect( process.env.CONNECTION_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-});
+  mongoose.connect( process.env.CONNECTION_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+  });
 
 // READ (Get request)
 app.get('/', (req, res) => {
