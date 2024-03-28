12065cce43e53c25d384d203259d28f8e777bc5e
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-03-28 11:02:27 -0700

    Add jwt token to /movies endpoint

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.get('/users/:username', passport.authenticate('jwt', {session: false }), asy
 });
 
 // READ (Get list of movies)
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', {session: false }), async (req, res) => {
   await Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
